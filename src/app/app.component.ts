4ddfe095a673e9fa65fd2ec51d497781a1df0691
hasnat-hybrid <hasnaties@gmail.com>
2022-08-17 01:46:36 +0500

    End of the Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularApp';
+  clicked:boolean;
+  logs: any;
+  itemNo: number;
+
+  constructor() {
+    this.clicked = false;
+    this.logs = [];
+    this.itemNo = 1;
+  }
+
+  buttonClick() {
+    this.clicked = true;
+    this.logs.push('Click No.' + this.itemNo)
+    this.itemNo = this.itemNo + 1;
+  }
+
+  logReached(){
+    return this.itemNo >= 5 ? true : false;
+  }
 }
